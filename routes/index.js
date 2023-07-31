e32c5c11f049f551f0a1abde45341d33fe8bc157
Arjun Santhosh <arjunsanthosh738@gmail.com>
2023-07-31 18:39:28 +0530

    Last

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('User/Home');
+});
+
+router.get('/about', function (req, res, next) {
   res.render('User/About');
+});
+
+router.get('/blog', function (req, res, next) {
+  res.render('User/Blog');
 });
 
 module.exports = router;
